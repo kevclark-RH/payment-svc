bdb3158cfaf6a818743e3f313b1618e1ad51e0ab
Andrew Baumann <ambaumann@users.noreply.github.com>
2016-07-29 14:24:04 -0700

    Added beginning of SOAP comp. changes

---
+++ payment-svc/src/test/java/org/payment/svc/TestCXFVSSoapComponents.java
@@ -0,0 +1,63 @@
+package org.payment.svc;
+
+import org.apache.camel.Produce;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.test.spring.CamelSpringTestSupport;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.AbstractXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.payment.models.Customer;
+
+public class TestCXFVSSoapComponents  extends CamelSpringTestSupport {
+
+	private Customer testCustomer;
+
+    @Produce(uri = "direct:callCXFEndpointSOAP") private ProducerTemplate soapFactory;
+    
+	
+    @Before
+    public void setup() throws Exception {
+    	
+    	//build test object
+    	//need some sort of wsdl object here that we can send
+    	
+    	testCustomer = new Customer();
+    	testCustomer.setFirstName("Andy");
+    	testCustomer.setLastName("Reed");
+    	testCustomer.setPhone("3847474789");
+    	testCustomer.setState("Arizona");
+    	testCustomer.setRegion("SW");
+    	testCustomer.setZip("44747");
+    	testCustomer.setCompanyName("AMEX");
+    }
+    
+    
+	@Test
+	public void testCamelRoute() throws Exception {
+		
+		
+		System.out.print("\n\n\n\nTESTING\n\n\n\n\n");
+		
+		//send to direct:endpoint
+		soapFactory.sendBody(testCustomer);
+
+
+		//dummy assert
+		assertTrue(true);
+	
+		
+	}
+
+
+
+	@Override
+	protected AbstractApplicationContext createApplicationContext() {
+		AbstractXmlApplicationContext springCtx	=
+				new ClassPathXmlApplicationContext("META-INF/spring/bundle-context.xml");
+		return springCtx;
+	}
+	
+}
