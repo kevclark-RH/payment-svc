8239ded5ed839537872a456f80857df53ec99511
Kevin Clark <kevclark@redhat.com>
2016-08-29 15:00:30 -0700

    servlet bind but method not found

--- parent/test.sh
+++ parent/test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-curl -H "Content-Type: application/json" -H "Accept=[application/json], Cache-Control=[no-cache], User-Agent=[Apache CXF 3.0.5], connection=[keep-alive], Pragma=[no-cache], type=[add], Content-Type=[application/json], Date=[Mon, 15 Aug 2016 18:27:29 GMT]" -X POST -d 'Customer {companyName=AMEX, region=SW, active=false, firstName=Andy, lastName=Reed, streetAddr=null, city=null, state=Arizona, zip=44747, phone=3847474789}' http://localhost:8080/PersonEJB/addCustomer 
-
+curl -H "Content-Type: application/json" -H "Accept=[application/json], Cache-Control=[no-cache], User-Agent=[Apache CXF 3.0.5], connection=[keep-alive], Pragma=[no-cache], type=[add], Content-Type=[application/json], Date=[Mon, 15 Aug 2016 18:27:29 GMT]" -X POST -d 'Customer {companyName=AMEX, region=SW, active=false, firstName=Andy, lastName=Reed, streetAddr=null, city=null, state=Arizona, zip=44747, phone=3847474789}' http://localhost:8080/payment-war/PersonEJB/addCustomer/ 
 
